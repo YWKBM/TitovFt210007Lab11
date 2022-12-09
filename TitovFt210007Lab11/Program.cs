c5124b7080d6175738ecd84e3819fc3eb050187a
YWKBM <ywkbm1973@gmail.com>
2022-12-09 15:59:48 +0500

    Добавьте файлы проекта.

---
+++ TitovFt210007Lab11/Program.cs
@@ -0,0 +1,21 @@
+﻿using Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TitovFt210007Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //тестовый запрос
+            GetRequest get = new GetRequest("https://restcountries.com/v3.1/capital/Moscow");
+            get.Run();
+            Console.WriteLine(get.Response);
+        }
+    }
+}
